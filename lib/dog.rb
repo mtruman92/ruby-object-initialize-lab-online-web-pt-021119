91d07a38c481227ebe93d27d3df897db0109a9ae
Mickae Truman <mtruman@nyit.edu>
2019-02-26 23:13:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
-  def initialize(breed, dogs_name)
+  def initialize(breed = "Mutt", dogs_name)
     @breed = breed
     @name = dogs_name
   end
 end
 
-def breed=(dogs_name, breed = "Mutt")
-  @name = dogs_name
-  @breed = breed
+#def breed=(dogs_name, breed = "Mutt")
+  #@name = dogs_name
+  #@breed = breed
 end
 
 #def breed=(breed = "Mutt",dogs_name)
